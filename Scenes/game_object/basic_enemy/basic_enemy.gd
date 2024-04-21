aa0de81a801404d1305595382b66cdd51ecdf876
ZoltanV <zoli0829@gmail.com>
2024-04-21 15:31:51 +0100

    Added more features

--- Scenes/game_object/basic_enemy/basic_enemy.gd
+++ Scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,14 +1,10 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 75
+const MAX_SPEED = 40
+
+@onready var health_component: HealthComponent = $HealthComponent
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
