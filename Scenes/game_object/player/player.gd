40ecac9d77506dce75227c754f96f145eff16e58
ZoltanV <zoli0829@gmail.com>
2024-04-27 11:20:13 +0100

    created an axe ability, enabled the aquisition of the axe ability, prevented abilities from being chosen twice, animated the player, animated the enemy, added a wizard enemy, used a weighted table for enemy spawning, animated the wizard

--- Scenes/game_object/player/player.gd
+++ Scenes/game_object/player/player.gd
@@ -6,6 +6,9 @@ const ACCELERATION_SMOOTHING = 25
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
+@onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 var number_colliding_bodies = 0
 
@@ -15,6 +18,7 @@ func _ready():
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 	health_component.health_changed.connect(on_health_changed)
+	GameEvents.ability_upgrades_added.connect(on_ability_upgrade_added)
 	update_health_display()
 
 
@@ -26,6 +30,15 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	
 	move_and_slide()
+	
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("RESET")
+	
+	var move_sign = sign(movement_vector.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, 1)
 
 
 func get_movement_vector():
@@ -61,3 +74,12 @@ func on_damage_interval_timer_timeout():
 
 func on_health_changed():
 	update_health_display()
+
+
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	# if it is just a normal ability upgrade
+	if not ability_upgrade is Ability:
+		return
+	
+	var ability = ability_upgrade as Ability
+	abilities.add_child(ability.ability_controller_scene.instantiate())
