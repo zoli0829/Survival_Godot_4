ae21474053f3ee0289b471cc4daaea917e9955b4
ZoltanV <zoli0829@gmail.com>
2024-05-03 20:32:08 +0100

    added a bat enemy

--- Scenes/manager/enemy_manager.gd
+++ Scenes/manager/enemy_manager.gd
@@ -5,6 +5,7 @@ const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
 @export var wizard_enemy_scene: PackedScene
+@export var bat_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
@@ -64,4 +65,6 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	timer.wait_time = base_spawn_time - time_off
 	
 	if arena_difficulty == 6:
-		enemy_table.add_item(wizard_enemy_scene, 20)
+		enemy_table.add_item(wizard_enemy_scene, 15)
+	elif arena_difficulty == 18:
+		enemy_table.add_item(bat_enemy_scene, 8)
