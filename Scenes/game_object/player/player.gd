3434203b2cf0a647b550b2bdeb3c1fcbf2861635
ZoltanV <zoli0829@gmail.com>
2024-05-05 12:04:55 +0100

    added an fps counter, and tweaked enemy spawning

--- Scenes/game_object/player/player.gd
+++ Scenes/game_object/player/player.gd
@@ -95,6 +95,6 @@ func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades:
 func on_arena_difficulty_increased(difficulty: int):
 	var health_regeneration_quantity = MetaProgression.get_upgrade_count("health_regeneration")
 	if health_regeneration_quantity > 0:
-		var is_thirty_second_interval = (difficulty % 6 ) == 0
+		var is_thirty_second_interval = (difficulty % 12 ) == 0
 		if is_thirty_second_interval:
 			health_component.heal(health_regeneration_quantity)
